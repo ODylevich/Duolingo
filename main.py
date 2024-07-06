ea5a3939bcc11062cf29bb26a6a64dd31c73e0c9
Aintreex <andrey.dylevich@gmail.com>
2024-07-06 18:48:02 +0300

    changes

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ class Card:
 
     def pick_card(self):
         self.picked_side = random.choice([self.word, self.translation])
-        return self.picked_side
+        print(self.picked_side)
+
+    def check_word(self):
+        user_answer = input("Write the word")
+        if (user_answer == self.word or user_answer == self.translation):
+            print("Well done ")
+        else:
+            print("Not well done")
+
+
+
+
+
+
+
 
 
 cards = []
@@ -17,11 +31,14 @@ cards = []
 new_words = {
     'Hola': 'Приветтт',
     'Adios': "Пока",
-    'Gracias': 'Спасибо'
+    'Gracias': 'Спасибо',
+    'Palabras': 'Слова'
 }
 
 for key, value in new_words.items():
     cards.append(Card(word=key, translation=value))
 
-print(f'Picked card function: {cards[0].pick_card()}')
-print(f'Picked side : {cards[0].picked_side}')
+
+random_index = random.randint(0,len(cards)-1)
+cards[random_index].pick_card()
+cards[random_index].check_word()
